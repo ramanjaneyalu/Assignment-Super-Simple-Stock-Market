b719a762d50dbe30180a8e4216983bc33560645b
Ramanjaneyalu <Ramanjaneyalu44@gmail.com>
2021-10-06 11:32:38 +0530

    updated test cases for buy and sell

--- unit_test.py
+++ unit_test.py
@@ -17,22 +17,26 @@ class UnitTests(unittest.TestCase):
 
     def test_check_pe_ratio(self):
         stock = self.stock_data.get("ALE")
-        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.PERatio, "ALE", 85)
+        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.PERatio,
+                                                                       "ALE", 85)
         pe_ratio_obj = PERatio(85, stock.get_last_dividend())
         expected_result = pe_ratio_obj.execute()
         self.assertEqual(result, expected_result)
 
     def test_dividend_yield_common(self):
         stock = self.stock_data.get("ALE")
-        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.DividendYield, "ALE", 185)
+        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.DividendYield,
+                                                                       "ALE", 185)
         dividend_yield_obj = CommonDividendYield(stock.get_last_dividend(), 185)
         expected_result = dividend_yield_obj.execute()
         self.assertEqual(result, expected_result)
 
     def test_dividend_yield_preferred(self):
         stock = self.stock_data.get("GIN")
-        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.DividendYield, "GIN", 185)
-        dividend_yield_obj = PreferredDividendYield(stock.get_fixed_dividend(), stock.get_par_value(), 185)
+        result = self.stock_entity_manager_obj.execute_formula_request(FormulaRequest.DividendYield,
+                                                                       "GIN", 185)
+        dividend_yield_obj = PreferredDividendYield(stock.get_fixed_dividend(), stock.get_par_value(),
+                                                    185)
         expected_result = dividend_yield_obj.execute()
         self.assertEqual(result, expected_result)
 
